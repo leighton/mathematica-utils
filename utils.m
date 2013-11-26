19f4674879e7eb0388300a3c88161e4dd96dfd05
Leighton Turner <leightonturner@gmail.com>
2013-11-26 13:18:10 +1300

    Square matrix test

--- utils.m
+++ utils.m
@@ -3,12 +3,22 @@ BeginPackage["Utils`"];
 Unprotect @@ Names["Utils`*"];
 ClearAll @@ Names["Utils`*"];
 
+SquareMatrixQ::usage = "gives True if expr is a list of lists or a 
+two-dimensional SparseArray object that can represent a matrix whose two
+dimensions are of the same order, and gives false otherwise";
+
 J::usage = "Compute the Jacobian of a system n equations by n variables";
 
 Begin["`Private`"]
 
+
+SquareMatrixQ[expr_] := 
+  MatrixQ[expr] && Length[Union[Dimensions[expr]]] == 1;
+
+
 J[eqs_: ListQ, vars_: ListQ] := 
  Outer[D, eqs, vars] /; Length[eqs] == Length[vars];
+
 
 End[ ]
 
